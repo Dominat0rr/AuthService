e7e1b3212a412c06e51019555f9a1cb2bd83c624
Dominat0rr <claermandominik@gmail.com>
2023-11-23 18:49:47 +0100

    Update TokenResponse.java

--- src/main/java/be/equals/authservice/controller/response/TokenResponse.java
+++ src/main/java/be/equals/authservice/controller/response/TokenResponse.java
@@ -2,13 +2,11 @@ package be.equals.authservice.controller.response;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class TokenResponse {
