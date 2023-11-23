536a3b36b828d3970d9abc954d35d70cf0e32963
Dominat0rr <claermandominik@gmail.com>
2023-11-23 18:49:43 +0100

    Update UserLoginRequest.java

--- src/main/java/be/equals/authservice/controller/request/UserLoginRequest.java
+++ src/main/java/be/equals/authservice/controller/request/UserLoginRequest.java
@@ -2,13 +2,11 @@ package be.equals.authservice.controller.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserLoginRequest {
