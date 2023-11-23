968a65371e47165174b8bc10923b6ef716e6e3e5
Dominat0rr <claermandominik@gmail.com>
2023-11-23 18:49:40 +0100

    Update AuthRestController.java

--- src/main/java/be/equals/authservice/controller/AuthRestController.java
+++ src/main/java/be/equals/authservice/controller/AuthRestController.java
@@ -1,6 +1,8 @@
 package be.equals.authservice.controller;
 
+import be.equals.authservice.controller.request.TokenRequest;
 import be.equals.authservice.controller.response.TokenResponse;
+import be.equals.authservice.controller.response.UserResponse;
 import be.equals.authservice.domain.User;
 import be.equals.authservice.exception.UserNotFoundException;
 import be.equals.authservice.repository.UserRepository;
@@ -10,10 +12,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth/token")
@@ -38,6 +37,34 @@ public class AuthRestController {
                     .token(jwtUtils.generateToken(user))
                     .build(),
                     HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    }
+
+    @PostMapping("/valid")
+    public ResponseEntity<?> isValidToken(@Valid @RequestBody TokenRequest tokenRequest) {
+        if (jwtUtils.isValidToken(tokenRequest.getToken()))
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/details")
+    public ResponseEntity<UserResponse> getUserDetails(@Valid @RequestBody TokenRequest tokenRequest) {
+        if (jwtUtils.isValidToken(tokenRequest.getToken())) {
+            final String username = jwtUtils.extractUsername(tokenRequest.getToken());
+            final User user = userRepository.findByEmail(username)
+                    .orElseThrow(() -> new UserNotFoundException(username));
+
+            final UserResponse userResponse = UserResponse.builder()
+                    .email(user.getEmail())
+                    .username(user.getRealUsername())
+                    .firstName(user.getFirstName())
+                    .lastName(user.getLastName())
+                    .build();
+
+            return new ResponseEntity<>(userResponse, HttpStatus.OK);
         }
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
