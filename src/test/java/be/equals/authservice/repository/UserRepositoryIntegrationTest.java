dfe378f39a3eefb24422e60ed2c6163b5a1fb93c
Dominat0rr <claermandominik@gmail.com>
2023-12-04 23:03:56 +0100

    Update UserRepositoryIntegrationTest.java

--- src/test/java/be/equals/authservice/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/be/equals/authservice/repository/UserRepositoryIntegrationTest.java
@@ -1,7 +1,60 @@
 package be.equals.authservice.repository;
 
-public class UserRepositoryIntegrationTest {
+import be.equals.authservice.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.jdbc.Sql;
 
-    // TODO:
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@Sql(UserRepositoryIntegrationTest.INIT_DB_SCRIPT)
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@EntityScan(basePackageClasses = User.class)
+class UserRepositoryIntegrationTest extends MySQLTestContainer {
+    protected static final String INIT_DB_SCRIPT = "classpath:users.sql";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private static final String EXISTING_EMAIL = "john@email.be";
+    private static final String NON_EXISTING_EMAIL = "test@email.be";
+
+    @Test
+    void contextLoads() {
+        assertThat(userRepository).isNotNull();
+    }
+
+    @Test
+    void whenFindByEmail_withExistingEmail_thenUserIsReturned() {
+        User expectedUser = createUser();
+
+        Optional<User> user = userRepository.findByEmail(EXISTING_EMAIL);
+
+        assertThat(user.get())
+                .usingRecursiveComparison()
+                .ignoringFields("password")
+                .isEqualTo(expectedUser);
+    }
+
+    @Test
+    void whenFindByEmail_withNonExsitingEmail_ThenOptionalEmptyIsReturned() {
+        Optional<User> user = userRepository.findByEmail(NON_EXISTING_EMAIL);
+
+        assertThat(user).isEqualTo(Optional.empty());
+    }
+
+    private static User createUser() {
+        return User.builder()
+                .id(100L)
+                .email("john@email.be")
+                .firstName("John")
+                .lastName("Doe")
+                .password("password")
+                .username("john")
+                .build();
+    }
 }
